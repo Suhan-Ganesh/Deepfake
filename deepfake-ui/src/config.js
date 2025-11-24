4a6ace1fbf9c7a747d6991af1bd63020edb0c781
Trishul <xxtrishulxx@gmail.com>
2025-11-25 01:34:18 +0530

    Before fixing view on blockchain

--- deepfake-ui/src/config.js
+++ deepfake-ui/src/config.js
@@ -1,4 +1,4 @@
 // Blockchain configuration
-export const CONTRACT_ADDRESS = "0xEc6c010Bf5B867e2b763D79FE21DFB9449a2e7a4";
+export const CONTRACT_ADDRESS = "0xa6961D0aa9ADb06E59C8d45B768E5901923b6EBF";
 export const NETWORK_ID = "11155111"; // Sepolia testnet
 export const NETWORK_NAME = "sepolia";
